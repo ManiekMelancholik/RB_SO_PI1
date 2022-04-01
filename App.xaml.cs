2ac01f683bff61cee72c2c3cabd26da5151526c1
Radosław Boczoń <88336041+ManiekMelancholik@users.noreply.github.com>
2022-04-01 23:28:52 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RB_SO_PI1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static public MainWindowMV mv;
+        public void InitApp(object o, StartupEventArgs e)
+        {
+            mv = new MainWindowMV(false);
+        }
+    }
+}
---
+++ CMM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RB_SO_PI1
+{
+    class CMM : ICommand
+    {
+
+
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+        public CMM(Action<object> ex, Func<object, bool> cex)
+        {
+            execute = ex;
+            canExecute = cex;
+
+
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return false;
+            }
+
+
+            return canExecute.Invoke(parameter);
+
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                execute(parameter);
+            }
+        }
+    }
+
+}
